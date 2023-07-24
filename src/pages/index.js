222795e82bceac6f36322d6b55d048d43f5034a8
Karol <kar.mal@icloud.com>
2023-07-24 11:38:38 +0100

    Adding first component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+import Hero from "../components/hero";
 
 const pageStyles = {
   color: "#232129",
@@ -126,6 +127,7 @@ const links = [
 const IndexPage = () => {
   return (
     <main style={pageStyles}>
+      <Hero />
       <h1 style={headingStyles}>
         This is our first build
         <br />
